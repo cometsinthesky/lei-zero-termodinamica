d807e8de05cfbe50a837053e3f1be27c2e7eac22
cometsinthesky <lucasferreiraunb@gmail.com>
2024-04-05 23:47:01 -0300

    create materials array for future optimization

---
+++ materials.js
@@ -0,0 +1,16 @@
+const materials = [
+    {
+        title: "Água",
+        id: "water"
+    },
+    {
+        title: "Gelo",
+        id: "ice"
+    },
+    {
+        title: "Vidro",
+        id: "glass"
+    }
+];
+
+module.exports = materials;
